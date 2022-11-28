afe997a54c53750819067b65949ecf306abcb785
Aigul <sharipova.aig@gmail.com>
2022-11-27 21:26:13 -0500

    start project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import image from './image.jpg'
+
+function App() {
+  return (
+    <div> 
+      <img src={image} alt='shopping' />
+      <h1>Grocery List</h1>
+    </div>
+  );
+}
+
+export default App;
