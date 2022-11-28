1104780cd8e84ef6c5a86f2f1350213039473a49
Aigul <sharipova.aig@gmail.com>
2022-11-28 08:24:11 -0500

    buttonAdd

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import imageTwo from './photo2.jpg'
 function App() {
   return (
     <div className='app'> 
-     <img src={image} width='250px' alt='shopping'/>
+    <img src={image} width='250px' alt='shopping'/>
     <h1>Shopping List</h1>
     <GroceryList />
     <img src={imageTwo} width='250px' alt='shopping main'/>
--- src/GroceruList.js
+++ src/GroceruList.js
@@ -13,11 +13,20 @@ export class GroceryList extends Component {
   render() {
     return(
       <div>
-      <input type='text'
-      placeholder="What do you want to buy?"
-      onChange={(e) => {this.onChangeEvent(e.target.value)}}
-      value={this.state.userInput}/>
-    </div>
+        <div>
+          <input type='text'
+          placeholder="What do you want to buy?"
+          onChange={(e) => {this.onChangeEvent(e.target.value)}}
+          value={this.state.userInput}/>
+        </div>
+        <div>
+          <button onClick={() => this.addItem(this.state.userInput)}>ADD</button>
+          </div>
+
+
+      </div>
+     
+    
     )
   
   }
