7fb9bbd921b38ede357d783b7fcdf056ea843cad
Aigul <sharipova.aig@gmail.com>
2022-11-28 08:08:22 -0500

    onChangeEvent

--- src/GroceruList.js
+++ src/GroceruList.js
@@ -4,16 +4,19 @@ export class GroceryList extends Component {
   state = {
     userInput:'',
     shoppingList:[]
-
   }
+
+  onChangeEvent(e){
+    this.setState({userInput:e})
+  }
+
   render() {
     return(
       <div>
       <input type='text'
-       placeholder="What do you want to buy?"
-      onChange={this.onChangeEvent}
-      value={this.state.userInput}
-      />
+      placeholder="What do you want to buy?"
+      onChange={(e) => {this.onChangeEvent(e.target.value)}}
+      value={this.state.userInput}/>
     </div>
     )
   
